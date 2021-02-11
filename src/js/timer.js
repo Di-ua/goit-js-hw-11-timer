d7c06f2d9423a7bac56e9cf746072b55ee3cbdae
Di-ua <di.ua@mail.ru>
2021-02-11 21:55:27 +0200

    trying add clear interval

--- src/js/timer.js
+++ src/js/timer.js
@@ -13,11 +13,39 @@ const secsRef = timerRef.querySelector('span[data-value="secs"]');
 
 
 setInterval(()=>{
+   // if (secs===0 && mins===0 && hours===0 && days===0) {
+   //    document.getElementById("countdown").className = "hidden";
+   //    document.getElementById("deadline-message").className = "visible";
+   //    document.getElementById("timer-1").className = "hidden";
+   //    clearInterval(date);
+   //    return true;
+   // }
     const time = date.targetDate - Date.now();
+
+    
+    /*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    /*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    /*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */ 
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
     const secs = Math.floor((time % (1000 * 60)) / 1000);
+    
     dayRef.textContent = days;
     hoursRef.textContent = hours;
     minsRef.textContent = mins;
